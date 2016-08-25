9d450663782592a07cf8496d12507ef644035659
Lyon Wu <haoliang.wu@liferay.com>
2016-08-25 11:06:15 +0800

    hot deploy

--- angular/src/index.js
+++ angular/src/index.js
@@ -1,3 +1,5 @@
 import angular from 'angular'
 
-angular.module('todo', [])
+angular.module('todomvc', [])
+
+if (module.hot) module.hot.accept()
--- angular/webpack.config.dev.js
+++ angular/webpack.config.dev.js
@@ -1,8 +1,9 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   context: __dirname + '/src',
-  entry: './index.js',
+  entry: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './index.js'],
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
@@ -14,6 +15,11 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, include: path.resolve(__dirname, 'src'), loader: 'babel-loader' }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 
 }
