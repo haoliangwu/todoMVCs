70f20cb162f0403a13c63073217155511922ed14
Lyon Wu <haoliang.wu@liferay.com>
2016-08-25 10:55:56 +0800

    structure

--- angular/src/index.js
+++ angular/src/index.js
@@ -1,1 +1,3 @@
-console.log(1)
+import angular from 'angular'
+
+angular.module('todo', [])
--- angular/webpack.config.dev.js
+++ angular/webpack.config.dev.js
@@ -1,8 +1,19 @@
+const path = require('path')
+
 module.exports = {
   context: __dirname + '/src',
   entry: './index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
+  },
+  externals: {
+    angular: 'angular'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, include: path.resolve(__dirname, 'src'), loader: 'babel-loader' }
+    ]
   }
+
 }
