859f1a0b26f7ccf8a5a01e90f292d8e4f5bcbc0d
Lyon Wu <haoliang.wu@liferay.com>
2016-09-12 18:04:07 +0800

    SF

--- ui-router2/app.js
+++ ui-router2/app.js
@@ -26,7 +26,7 @@ app.use(require('webpack-hot-middleware')(compiler, {
 
 app.use('/json', express.static('src/json'))
 
-app.get('/index', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'src/index.html'))
 })
 
