83a5f417fbef8f32f8f811782b0072aafaccf1ec
Lyon Wu <haoliang.wu@liferay.com>
2016-09-27 16:10:46 +0800

    input all done

--- angular-directives/index.js
+++ angular-directives/index.js
@@ -2,4 +2,11 @@ angular.module('app', [])
   .controller('Controller', function () {
     this._input = 'i am input'
     this._checkbox = 'YES'
+    this._date = new Date(2013, 5, 14, 12, 30, 0)
+    this._email = 'a@b.c'
+    this._number = 514
+    this._radio = 'Foo'
+    this._range = 50
+    this._text = 'abc'
+    this._url = 'http://google.com'
   })
