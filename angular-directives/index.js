be5b6847a4f1a93b897f443303cdb0cd9ca19994
Lyon Wu <haoliang.wu@liferay.com>
2016-09-28 15:44:39 +0800

    ng-****

--- angular-directives/index.js
+++ angular-directives/index.js
@@ -1,5 +1,9 @@
-angular.module('app', [])
-  .controller('Controller', function () {
+angular.module('app', ['ngSanitize'])
+  .controller('Controller', ['$scope', function ($scope) {
+    this._cb = function (e) {
+      console.log(e + ' event triggers.')
+    }
+
     this._input = 'i am input'
     this._checkbox = 'YES'
     this._date = new Date(2013, 5, 14, 12, 30, 0)
@@ -9,4 +13,23 @@ angular.module('app', [])
     this._range = 50
     this._text = 'abc'
     this._url = 'http://google.com'
-  })
+
+    this._ngBind = 'ngBind'
+    this._ngBindHtml = '<a href="#">ngBindHtml</a>'
+    this._ngBindTemplate = 'ngBindTemplate'
+    this._ngChecked = true
+    this._ngClass = 'red'
+    this._ngClassEven = 'red'
+    this._ngClassOdd = 'blue'
+    this._ngHide = false
+    this._ngHref = 'www.google.com'
+    this._ngIf = true
+    this._ngList = []
+    this._ngModelOptions = 'ngModelOptions'
+    this._cancel = function (e) {
+      // Esc keycode is 27
+      if (e.keyCode === 27) {
+        $scope.myForm1._ngModelOpts.$rollbackViewValue()
+      }
+    }
+  }])
