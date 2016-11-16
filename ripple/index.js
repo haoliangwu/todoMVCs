4c34f6fa18228a2e88d30bb07408fe5a76d97eef
Lyon Wu <wuhaoliang@cloudist.cc>
2016-11-16 09:58:51 +0800

    record

--- ripple/index.js
+++ ripple/index.js
@@ -1,25 +1,23 @@
 import _ from 'lodash'
 import $ from 'jquery'
 
-document.write('<link rel="stylesheet" href="https://npmcdn.com/hack@0.5.2" />')
+import './main.css'
 
-const projectName = "ripple"
 
-document.body.className = 'hack'
+$('#app').click(function (e) {
+  const divRipple = $('<div class="ripple"></div>')
 
-const div = document.createElement('div')
-div.id = 'app'
-div.className = 'container'
-div.innerHTML = `
-  <h1>${projectName}</h1>
-  <div class="main">
-    <p>
-      Hi there, you've made a perfect start using tooling!
-    </p>
-    <p>
-      Check out <strong>README.md</strong> or head to <a href="https://github.com/egoist/tooling">tooling</a> to get more instructions.
-    </p>
-  </div>
-`
-document.body.appendChild(div)
+  const {left, top} = $(this).offset()
 
+  divRipple.css('left', e.pageX - left + 'px')
+  divRipple.css('top', e.pageY - top + 'px')
+  $(this).append(divRipple)
+
+  setTimeout(() => {
+    divRipple.addClass('active')
+  }, 100)
+
+  setTimeout(() => {
+    divRipple.remove()
+  }, 2100)
+})
