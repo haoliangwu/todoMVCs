c9214202a03687c311b35aa299704f0efcb8d5ad
Lyon <wuhaoliang@cloudist.cc>
2017-03-29 10:59:53 +0800

    [ADD] handsontable 插件模板

---
+++ handsontable-demo/index.js
@@ -0,0 +1,25 @@
+import $ from 'jquery'
+import Handsontable from 'handsontable/dist/handsontable.full'
+import 'handsontable/dist/handsontable.full.css'
+
+import {MyExternalPluginSkeleton} from './plugin'
+
+$('<div id="example"></div>').appendTo(document.body)
+
+var data = [
+  ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
+  ['2016', 10, 11, 12, 13],
+  ['2017', 20, 11, 14, 13],
+  ['2018', 30, 15, 12, 13]
+]
+
+Handsontable.plugins.registerPlugin('externalPluginSkeleton', MyExternalPluginSkeleton)
+
+var container = document.getElementById('example')
+var hot = new Handsontable(container, {
+  data: data,
+  rowHeaders: true,
+  colHeaders: true,
+  dropdownMenu: true,
+  foo: true
+})
---
+++ handsontable-demo/plugin.js
@@ -0,0 +1,56 @@
+import Handsontable from 'handsontable/dist/handsontable.full'
+
+export class MyExternalPluginSkeleton extends Handsontable.plugins.BasePlugin {
+  constructor (hotInstance) {
+    super(hotInstance)
+
+    this.foo = 'abc'
+  }
+
+  /**
+   * Check if the plugin is enabled in the Handsontable settings.
+   *
+   * @returns {Boolean}
+   */
+  isEnabled () {
+    return !!this.hot.getSettings().foo
+  }
+
+  /**
+   * Enable plugin for this Handsontable instance.
+   */
+  enablePlugin () {
+    if (this.enabled) {
+      return
+    }
+
+    this.addHook('afterChange', (changes, source) => {
+      if (!changes) return
+
+      console.log(changes, source)
+      console.log(this.foo)
+    })
+
+    super.enablePlugin()
+  }
+
+  /**
+   * Update plugin for this Handsontable instance.
+   */
+  updatePlugin () {
+    this.disablePlugin()
+    this.enablePlugin()
+    super.updatePlugin()
+  }
+
+  /**
+   * Disable plugin for this Handsontable instance.
+   */
+  disablePlugin () {
+    super.disablePlugin()
+  }
+
+  destroy () {
+    super.destroy()
+  }
+}
