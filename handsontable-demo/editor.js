0b39208038f8fef35b98f6c1d21c7f56ca38f021
Lyon <wuhaoliang@cloudist.cc>
2017-03-30 09:15:27 +0800

    [IMP] editor event hook

--- handsontable-demo/editor.js
+++ handsontable-demo/editor.js
@@ -22,10 +22,13 @@ export class SelectEditor extends Handsontable.editors.BaseEditor {
     super(...args)
 
     this._prepare = super.prepare.bind(this)
+    this.onBeforeKeyDown = this.onBeforeKeyDown.bind(this)
   }
 
   init () {
     console.log(this)
+    console.log(Handsontable.helper)
+    console.log(Handsontable)
 
   // Create detached node, add CSS class and make sure its not visible
     this.select = document.createElement('SELECT')
@@ -79,21 +82,40 @@ export class SelectEditor extends Handsontable.editors.BaseEditor {
 
   open () {
     var width = Handsontable.Dom.outerWidth(this.TD)
+  // important - group layout reads together for better performance
     var height = Handsontable.Dom.outerHeight(this.TD)
     var rootOffset = Handsontable.Dom.offset(this.instance.rootElement)
     var tdOffset = Handsontable.Dom.offset(this.TD)
+    var editorSection = this.checkEditorSection()
+    var cssTransformOffset
 
-  // sets select dimensions to match cell size
-    this.select.style.height = height + 'px'
-    this.select.style.minWidth = width + 'px'
+    switch (editorSection) {
+      case 'top':
+        cssTransformOffset = Handsontable.Dom.getCssTransform(this.instance.view.wt.wtScrollbars.vertical.clone.wtTable.holder.parentNode)
+        break
+      case 'left':
+        cssTransformOffset = Handsontable.Dom.getCssTransform(this.instance.view.wt.wtScrollbars.horizontal.clone.wtTable.holder.parentNode)
+        break
+      case 'corner':
+        cssTransformOffset = Handsontable.Dom.getCssTransform(this.instance.view.wt.wtScrollbars.corner.clone.wtTable.holder.parentNode)
+        break
+    }
+    var selectStyle = this.select.style
 
-  // make sure that list positions matches cell position
-    this.select.style.top = tdOffset.top - rootOffset.top + 'px'
-    this.select.style.left = tdOffset.left - rootOffset.left + 'px'
-    this.select.style.margin = '0px'
+    if (cssTransformOffset && cssTransformOffset !== -1) {
+      selectStyle[cssTransformOffset[0]] = cssTransformOffset[1]
+    } else {
+      Handsontable.Dom.resetCssTransform(this.select)
+    }
 
-  // display the list
-    this.select.style.display = ''
+    selectStyle.height = height + 'px'
+    selectStyle.minWidth = width + 'px'
+    selectStyle.top = tdOffset.top - rootOffset.top + 'px'
+    selectStyle.left = tdOffset.left - rootOffset.left + 'px'
+    selectStyle.margin = '0px'
+    selectStyle.display = ''
+
+    this.instance.addHook('beforeKeyDown', this.onBeforeKeyDown)
   }
 
   focus () {
@@ -111,6 +133,36 @@ export class SelectEditor extends Handsontable.editors.BaseEditor {
 
   close () {
     this.select.style.display = 'none'
+    this.instance.removeHook('beforeKeyDown', this.onBeforeKeyDown)
+  }
+
+  // event hook
+  onBeforeKeyDown (event) {
+    var editor = this.instance.getActiveEditor()
+    var selectedIndex = editor.select.selectedIndex
+
+    switch (event.keyCode) {
+      case Handsontable.helper.KEY_CODES.ARROW_UP:
+        var previousOption = editor.select.options[selectedIndex - 1]
+
+        if (previousOption) { // if previous option exists
+          editor.select.value = previousOption.value // mark it as selected
+        }
+
+        event.stopImmediatePropagation() // prevent EditorManager from processing this event
+        event.preventDefault() // prevent browser from scrolling the page up
+        break
+
+      case Handsontable.helper.KEY_CODES.ARROW_DOWN:
+        var nextOption = editor.select.options[selectedIndex + 1]
+
+        if (nextOption) { // if previous option exists
+          editor.select.value = nextOption.value // mark it as selected
+        }
+        event.stopImmediatePropagation() // prevent EditorManager from processing this event
+        event.preventDefault() // prevent browser from scrolling the page up
+        break
+    }
   }
 }
 
