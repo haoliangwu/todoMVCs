c0de3d65ec15364c4f56874e4621a3e00fc5707e
Lyon <wuhaoliang@cloudist.cc>
2017-07-20 16:42:38 +0800

    sf

--- angular2-ngModules/src/app/app.module.ts
+++ angular2-ngModules/src/app/app.module.ts
@@ -1,26 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
+/* App Root */
 import { AppComponent } from './app.component'
+import { HighlightDirective } from './highlight.directive'
 import { TitleComponent } from './title/title.component'
 import { UserService } from './user.service'
-import { ContactComponent } from './contact/contact.component'
-import { HighlightDirective } from './hightlight.directive'
-import { ContactModule } from './contact/contact.module'
+
+/* Contact Imports */
+import
+{ ContactModule } from './contact/contact.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HighlightDirective,
-    TitleComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ContactModule
-  ],
+  imports: [BrowserModule, ContactModule],
+  declarations: [AppComponent, HighlightDirective, TitleComponent],
   providers: [UserService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
