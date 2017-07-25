8d4e9afac46e65a2666327b8e8050f769c9ddb2a
Lyon <wuhaoliang@cloudist.cc>
2017-07-25 21:53:34 +0800

    formArray

--- angular2-react-forms/src/app/hero-detail/hero-detail.component.ts
+++ angular2-react-forms/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { states, Address, Hero } from './data-model';
 
 @Component({
@@ -12,6 +12,10 @@ export class HeroDetailComponent implements OnChanges {
   heroForm: FormGroup;
   states = states;
 
+  get secretLairs(): FormArray {
+    return this.heroForm.get('secretLairs') as FormArray;
+  };
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -21,14 +25,34 @@ export class HeroDetailComponent implements OnChanges {
       name: this.hero.name,
       address: this.hero.addresses[0] || new Address()
     });
+    this.setAddresses(this.hero.addresses);
   }
 
   createForm() {
     this.heroForm = this.fb.group({ // <-- the parent FormGroup
       name: ['', Validators.required],
-      address: this.fb.group(new Address()),
+      // address: this.fb.group(new Address()),
+      secretLairs: this.fb.array([]),
       power: '',
       sidekick: ''
     });
+  }
+
+  setAddresses(addresses: Address[]) {
+    const addressFGs = addresses.map(address => this.fb.group(address));
+    const addressFormArray = this.fb.array(addressFGs);
+    this.heroForm.setControl('secretLairs', addressFormArray);
+  }
+
+  addLair() {
+    this.secretLairs.push(this.fb.group(new Address()));
+  }
+
+  removeLair(){
+    this.secretLairs.removeAt(this.secretLairs.length - 1)
+  }
+
+  resetLair(){
+    this.ngOnChanges()
   }
 }
