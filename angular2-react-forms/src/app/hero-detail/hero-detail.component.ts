867c9b5c54684dc27eab9a08d0e910ed6958b366
Lyon <wuhaoliang@cloudist.cc>
2017-07-25 22:04:23 +0800

    save and revert

--- angular2-react-forms/src/app/hero-detail/hero-detail.component.ts
+++ angular2-react-forms/src/app/hero-detail/hero-detail.component.ts
@@ -62,7 +62,33 @@ export class HeroDetailComponent implements OnChanges {
     this.secretLairs.removeAt(this.secretLairs.length - 1)
   }
 
-  resetLair() {
+  revert() {
     this.ngOnChanges()
   }
+
+  prepareSaveHero(): Hero {
+    const formModel = this.heroForm.value;
+
+    // deep copy of form model lairs
+    const secretLairsDeepCopy: Address[] = formModel.secretLairs.map(
+      (address: Address) => Object.assign({}, address)
+    );
+
+    // return new `Hero` object containing a combination of original hero value(s)
+    // and deep copies of changed form model values
+    const saveHero: Hero = {
+      id: this.hero.id,
+      name: formModel.name as string,
+      // addresses: formModel.secretLairs // <-- bad!
+      addresses: secretLairsDeepCopy
+    };
+    return saveHero;
+  }
+
+  onSubmit() {
+  this.hero = this.prepareSaveHero();
+  // this.heroService.updateHero(this.hero).subscribe(/* error handling */);
+  this.ngOnChanges();
+  alert('saved hero')
+}
 }
