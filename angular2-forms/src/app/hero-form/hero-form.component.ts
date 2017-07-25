732e0dbe3da1bbd9eecafe0bc7c9e55ffb065d7e
Lyon <wuhaoliang@cloudist.cc>
2017-07-25 17:04:06 +0800

    refactor form with simple validation

--- angular2-forms/src/app/hero-form/hero-form.component.ts
+++ angular2-forms/src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Hero } from './hero';
 
@@ -8,9 +9,62 @@ import { Hero } from './hero';
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent {
+  // version 2
+  heroForm: NgForm
+  @ViewChild('heroForm') currentForm: NgForm;
 
   powers = ['Really Smart', 'Super Flexible',
     'Super Hot', 'Weather Changer'];
+
+  validationMessages = {
+    'name': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 4 characters long.',
+      'maxlength': 'Name cannot be more than 24 characters long.',
+      'forbiddenName': 'Someone named "Bob" cannot be a hero.'
+    },
+    'power': {
+      'required': 'Power is required.'
+    }
+  };
+
+  formErrors = {
+    'name': '',
+    'power': ''
+  };
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    if (this.currentForm === this.heroForm) { return; }
+    this.heroForm = this.currentForm;
+    if (this.heroForm) {
+      this.heroForm.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+    }
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.heroForm) { return; }
+    const form = this.heroForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  // version 1
 
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
