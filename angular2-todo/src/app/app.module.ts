5a18977c0a09460a52f9b6e4d53475905144fbba
Lyon <wuhaoliang@cloudist.cc>
2017-08-06 22:20:14 +0800

    simple route integrate

--- angular2-todo/src/app/app.module.ts
+++ angular2-todo/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { TodoListAppModule } from './todo-list-app/todo-list-app.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    TodoListAppModule
+    TodoListAppModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
